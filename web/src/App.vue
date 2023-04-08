746fe9d785a4dbca4b6eed2cc9623f8fd700b318
736327363@qq.com <736327346@qq.com>
2023-04-08 22:03:06 +0800

    制作the-footer组件

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,21 +1,21 @@
 <template>
   <a-layout>
-    <the-header/>
+    <the-header></the-header>
     <router-view/>
-    <a-layout-footer style="text-align: center">
-      LinEbook
-    </a-layout-footer>
+    <the-footer></the-footer>
   </a-layout>
 </template>
 
 <script lang="ts">
 import {defineComponent} from "vue";
 import TheHeader from "@/components/the-header.vue";
+import TheFooter from "@/components/the-footer.vue";
 
 export default defineComponent({
   name: 'app',
   components: {
     TheHeader,
+    TheFooter,
   }
 })
 </script>
--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -55,11 +55,10 @@
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 @Options({
   components: {
-    HelloWorld,
+
   },
 })
 export default class HomeView extends Vue {}
